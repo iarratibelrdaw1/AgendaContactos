022dabf565172d94df97a042b242a0caaad6f171
iarratibelrdaw1 <iarratibelrdaw1@gmail.com>
2021-03-30 13:42:20 +0200

    Metodo personalesPorRelacion a medias

--- src/ut7/agenda/io/AgendaIO.java
+++ src/ut7/agenda/io/AgendaIO.java
@@ -4,6 +4,11 @@ import ut7.agenda.modelo.*;
 
 /**
  * Utilidades para cargar la agenda
+ * 
+ * @author Alex Calderón, Irune Arratibel, Daniel Jiménez
+ * @version 1.0
+ *
+ 
  */
 public class AgendaIO {
 
