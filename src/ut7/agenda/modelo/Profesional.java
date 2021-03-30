aa0c6fb2bf50e360e83369273d75b51cc9784abb
iarratibelrdaw1 <iarratibelrdaw1@gmail.com>
2021-03-30 12:28:16 +0200

    La clase Profesional con comentarios

--- src/ut7/agenda/modelo/Profesional.java
+++ src/ut7/agenda/modelo/Profesional.java
@@ -10,42 +10,70 @@ package ut7.agenda.modelo;
 public class Profesional extends Contacto {
 	private String nombreEmpresa;
 
+	/**
+	 * Constructor
+	 * 
+	 * Crea un objeto Profesional
+	 * 
+	 * @param nombre
+	 * @param apellidos
+	 * @param telefono
+	 * @param email
+	 * @param nombreEmpresa
+	 */
+	
 	public Profesional(String nombre, String apellidos, String telefono, String email, String nombreEmpresa) {
 		super(nombre, apellidos, telefono, email);
 		this.nombreEmpresa = nombreEmpresa;
 	}
 
+	/**
+	 * Devuelve el nombre de la empresa (Getter)
+	 * @return String con el nombre de la empresa
+	 */
 	public String getNombreEmpresa() {
 		return nombreEmpresa;
 	}
 
+	/**
+	 * Cambia el nombre de la empresa al parámetro introducido (Setter)
+	 * @param nombreEmpresa
+	 */
 	public void setNombreEmpresa(String nombreEmpresa) {
 		this.nombreEmpresa = nombreEmpresa;
 	}
+	
+	/**
+	 * Saca la firma del email
+	 * @return String con la firma
+	 */
+		@Override
+		public String getFirmaEmail() {
+			String resul="";
+			int aux = (int)(Math.random() * 4 + 1);
+			switch(aux) {
+				case 1:
+					resul = "Atentamente";
+				case 2:
+					resul = "Saludos";
+				case 3:
+					resul = "Saludos cordiales";
+				case 4:
+					resul = "Mis mejores deseos";	
+			}
+			return resul;
+		}
+		
 
+	/**
+	 * Representación textual del contacto Profesional
+	 * @return String con el texto
+	 */
 	@Override
 	public String toString() {
-		return (super.toString() + "\nEmpresa: " + nombreEmpresa);
+		return (super.toString() + "\nEmpresa: " + nombreEmpresa + "\n");
 				
 	}
-	
-	//@Override
-	public String getFirmaEmail() {
-		String resul="";
-		int aux = (int)(Math.random() * 4 + 1);
-		switch(aux) {
-			case 1:
-				resul = "Atentamente";
-			case 2:
-				resul = "Saludos";
-			case 3:
-				resul = "Saludos cordiales";
-			case 4:
-				resul = "Mis mejores deseos";	
-		}
-		return resul;
-	}
-	
 	
 
 }
